2c943664b83235cef48018c661ccb3a2d56049d4
sandeepmshetty <sandeepmshetty@gmail.com>
2020-08-28 14:15:35 +0530

    Job stack added

--- src/components/Body/DashboardTabView/DashboardTabView.jsx
+++ src/components/Body/DashboardTabView/DashboardTabView.jsx
@@ -12,29 +12,132 @@ export default class DashboardTabView extends Component {
         this.state = {
             columnDefs: [
                 {
-                    headerName: "Make",
-                    field: "make"
+                    headerName: "Req",
+                    field: "req",
+                    sortable: true,
+                    filter: true
                 }, {
-                    headerName: "Model",
-                    field: "model"
+                    headerName: "Job No",
+                    field: "jobNo",
+                    sortable: true,
+                    filter: true
+
                 }, {
-                    headerName: "Price",
-                    field: "price"
+                    headerName: "Researcher",
+                    field: "researcher",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Client",
+                    field: "client",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Region",
+                    field: "region",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Sector",
+                    field: "sector",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Status",
+                    field: "status",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Job Type",
+                    field: "jobType",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Team",
+                    field: "team",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Details",
+                    field: "details",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Comment",
+                    field: "comment",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Time In",
+                    field: "timeIn",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Time Due",
+                    field: "timeDue",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "Due In",
+                    field: "dueIn",
+                    sortable: true,
+                    filter: true
+                }, {
+                    headerName: "ETR",
+                    field: "etr",
+                    sortable: true,
+                    filter: true
                 }
             ],
             rowData: [
                 {
-                    make: "Toyota",
-                    model: "Celica",
-                    price: 35000
+                    req: "38661",
+                    jobNo: "348515",
+                    researcher: 'Shashi',
+                    client: 'Sandeep S',
+                    region: 'EMEA',
+                    sector: 'Other',
+                    status: 'New',
+                    jobType: 'CI-Other',
+                    team: 'IMS-Mumbai',
+                    details: '',
+                    comment: '',
+                    timeIn: '20 Aug 2020 19:48',
+                    timeDue: '20 Aug 2020 19:48',
+                    dueIn: '',
+                    etr: ''
                 }, {
-                    make: "Ford",
-                    model: "Mondeo",
-                    price: 32000
+                    req: "386614",
+                    jobNo: "348511",
+                    researcher: 'Sanjay',
+                    client: 'Shashi S',
+                    region: 'EMEA',
+                    sector: 'Other',
+                    status: 'New',
+                    jobType: 'CI-Other',
+                    team: 'IMS-Mumbai',
+                    details: '',
+                    comment: '',
+                    timeIn: '20 Aug 2020 19:48',
+                    timeDue: '20 Aug 2020 19:48',
+                    dueIn: '',
+                    etr: ''
                 }, {
-                    make: "Porsche",
-                    model: "Boxter",
-                    price: 72000
+                    req: "386611",
+                    jobNo: "348509",
+                    researcher: 'Sanjay',
+                    client: 'Sandeep S',
+                    region: 'EMEA',
+                    sector: 'Other',
+                    status: 'New',
+                    jobType: 'CI-Other',
+                    team: 'IMS-Mumbai',
+                    details: '',
+                    comment: '',
+                    timeIn: '20 Aug 2020 19:48',
+                    timeDue: '20 Aug 2020 19:48',
+                    dueIn: '',
+                    etr: ''
                 }
             ]
         }
@@ -49,14 +152,14 @@ export default class DashboardTabView extends Component {
         const {TabPane} = Tabs;
 
         return (
-            <div className="bis-action-view">
+            <div className="bis-dashboard-tab-view">
                 <Tabs defaultActiveKey="1" onChange={this.callback}>
                     <TabPane tab="Job Stack" key="1">
                         <div
                             className="ag-theme-alpine"
                             style={{
-                            height: '250px',
-                            width: '600px'
+                            height: '450px',
+                            width: '100%'
                         }}>
                             <AgGridReact columnDefs={this.state.columnDefs} rowData={this.state.rowData}></AgGridReact>
                         </div>
