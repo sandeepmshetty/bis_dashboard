faac40d507c98e34caf32d784403f2894401a58b
sandeepmshetty <sandeepmshetty@gmail.com>
2020-09-21 17:44:30 +0530

    Cell rendering implemented

--- src/components/Body/DashboardTabView/DashboardTabPaneView/JobStackTabPane.jsx
+++ src/components/Body/DashboardTabView/DashboardTabPaneView/JobStackTabPane.jsx
@@ -34,7 +34,6 @@ export default class JobStackTabPane extends Component {
           hide: false,
           cellRenderer: function(params) {
             let keyData = params.data.jobNo;
-            console.log(params);
             let newLink = 
             `<span><span role="img" aria-label="link" class="anticon anticon-link"><svg viewBox="64 64 896 896" focusable="false" class="" data-icon="link" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M574 665.4a8.03 8.03 0 00-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8a8.03 8.03 0 00-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zm258.6-474c-84.6-84.6-221.5-84.6-306 0L410.3 307.6a8.03 8.03 0 000 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6a8.03 8.03 0 000 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1zM610.1 372.3a8.03 8.03 0 00-11.3 0L372.3 598.7a8.03 8.03 0 000 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z"></path></svg></span><a href= https://ag-grid.com/
             target="_blank">${keyData}</a></span>`;
@@ -96,7 +95,12 @@ export default class JobStackTabPane extends Component {
           sortable: true,
           filter: true,
           hide: false,
+          cellRenderer: function(params) {
+            let newLink = 
+            `<span role="img" aria-label="info-circle" class="anticon anticon-info-circle"><svg viewBox="64 64 896 896" focusable="false" class="" data-icon="info-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path><path d="M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z"></path></svg></span>`;
+            return newLink;
         },
+      },
         {
           headerName: "Comment",
           field: "comment",
@@ -117,6 +121,7 @@ export default class JobStackTabPane extends Component {
           sortable: true,
           filter: true,
           hide: false,
+          cellClass: function(params) { return ('bis-maroon-cell'); }
         },
         {
           headerName: "Due In",
